c00cb3ab52b0ac9579c9b1c2223811df393fabe7
DenisFedorovich <denis_greben1991@mail.ru>
2019-10-13 21:25:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider1').slick({
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
